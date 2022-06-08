73233170330396bb2c809572cbf28fab309625bf
sedtia-prakoso-budi-tirto-arto <prakosobudi2k@gmail.com>
2022-06-08 18:57:30 +0700

    yok bisa yokk

--- app/Views/current.php
+++ app/Views/current.php
@@ -23,5 +23,21 @@
             <div class="fs-5 mt-2 fw-semibold border-bottom pb-3" style="margin-left: 50pt;">Vol 1, No 1 (2022)</div>
             <div class="fs-5 mt-2" style=" margin-left: 50pt;">Table of Contents</div>
             <div class="fs-6 mt-2" style=" margin-left: 50pt;">Articles</div>
+            <table class="tocArticle" width="100%" style="margin-left: 50pt; font-size:10pt;">
+                <tbody>
+                    <tr valign="top">
+                        <td class="tocTitle"><a href="https://iptek.its.ac.id/index.php/itj/article/view/12515">Flexural And Shear Behaviour Of Reinforced Concrete Slab With Pvc Pipe As A Cavity Forming In Two-Way System</a></td>
+                        <td class="tocGalleys">
+                            <a href="https://iptek.its.ac.id/index.php/itj/article/view/12515/6563" class="file">PDF</a>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="tocAuthors">
+                            &nbsp; &nbsp; &nbsp; &nbsp; <i>Cyntia Niani, Daniel Siahaan</i> </td>
+                        <td class="tocPages">1-10</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
